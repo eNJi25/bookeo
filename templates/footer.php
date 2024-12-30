4fe623be1c2ed6b3f1a0d77817ec7882bc5579c9
eNJi25 <nfischer.contact@gmail.com>
2024-12-30 22:12:25 +0100

    feat: création des templates header, footer, et home

---
+++ templates/footer.php
@@ -0,0 +1,16 @@
+        </main>
+
+        <footer class="py-3 my-4">
+            <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-primary">Home</a></li>
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-primary">Features</a></li>
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-primary">Pricing</a></li>
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-primary">FAQs</a></li>
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-primary">About</a></li>
+            </ul>
+            <p class="text-center text-body-primary">© 2024 Company, Inc</p>
+        </footer>
+        </div>
+        </body>
+
+        </html>

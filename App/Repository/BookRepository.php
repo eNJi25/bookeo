0780927562125df7ca00ba0256806c4da7877b35
eNJi25 <nfischer.contact@gmail.com>
2025-01-01 21:04:56 +0100

    add pdo config

--- App/Repository/BookRepository.php
+++ App/Repository/BookRepository.php
@@ -3,22 +3,32 @@
 namespace App\Repository;
 
 use App\Entity\Book;
+use App\Db\Mysql;
 
 class BookRepository
 {
     public function findOneById(int $id)
     {
-        // Ici normalement Appel BDD, la on créer en dur un obket puis on le retourne pour le test
+        $mysql = Mysql::getInstance();
+        $pdo = $mysql->getPDO();
+        $query = $pdo->prepare('SELECT * FROM book WHERE id = :id');
+        $query->bindValue(':id', $id, $pdo::PARAM_INT);
+        $query->execute();
+        $book = $query->fetch();
+
+        /* Ici  on créer en dur un obket puis on le retourne pour le test
         $book = [
             'id' => 1,
             'title' => 'Titre test',
             'description' => 'Ceci est un test'
         ];
+        */
 
         $bookEntity = new Book();
         $bookEntity->setId($book['id']);
         $bookEntity->setTitle($book['title']);
         $bookEntity->setDescription($book['description']);
+
 
         return $bookEntity;
     }
---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'db_name' => 'studi_bookeo',
+    'db_user' => 'test',
+    'db_password' => 'test',
+    'db_port' => '3306',
+    'db_host' => 'localhost'
+];
